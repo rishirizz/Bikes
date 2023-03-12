2bbb4d6b5c15a4cd27bc70fb7b64a6a26c77d946
arijeet <arijeet@sbox.ltd>
2023-03-12 12:31:04 +0530

    rough commit

--- lib/screens/bike_details.dart
+++ lib/screens/bike_details.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import '../models/bike_model.dart';
@@ -25,18 +27,92 @@ class _BikeDetailsScreenState extends State<BikeDetailsScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Colors.black,
         body: Column(
           children: [
             Expanded(
-              flex: 1,
+              flex: 2,
               child: Stack(
-                children: [],
+                children: [
+                  Image.asset(
+                    bike!.bikeImage!,
+                    fit: BoxFit.cover,
+                    filterQuality: FilterQuality.high,
+                    width: MediaQuery.of(context).size.width,
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(
+                        sigmaX: 4,
+                        sigmaY: 2,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            bike!.bikeName!,
+                            style: const TextStyle(
+                              fontSize: 40,
+                              color: Colors.white,
+                              letterSpacing: 2,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            width: 70,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(25.0),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Row(
+                                children: [
+                                  const Icon(
+                                    Icons.star_rounded,
+                                    color: Color.fromARGB(255, 144, 156, 234),
+                                    size: 19,
+                                  ),
+                                  const SizedBox(
+                                    width: 4,
+                                  ),
+                                  Text(
+                                    bike!.rating!,
+                                    style: const TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
             Expanded(
               flex: 3,
               child: Container(
-                color: Colors.white,
+                width: MediaQuery.of(context).size.width,
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 144, 156, 234),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25),
+                    topRight: Radius.circular(25),
+                  ),
+                ),
               ),
             ),
           ],
